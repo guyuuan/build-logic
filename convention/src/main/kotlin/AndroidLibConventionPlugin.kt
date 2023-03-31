0d7a2ab43ae80fe271c189306a00d1b67ed2edb1
chunjinchen <chunjinchen1998@gmail.com>
2023-03-31 10:17:14 +0800

    set library default proguard files

--- convention/src/main/kotlin/AndroidLibConventionPlugin.kt
+++ convention/src/main/kotlin/AndroidLibConventionPlugin.kt
@@ -2,10 +2,8 @@ import cn.chitanda.app.imovie.configureKotlinAndroid
 import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.kotlin
 
 /**
@@ -24,6 +22,9 @@ class AndroidLibConventionPlugin : Plugin<Project> {
             extensions.configure<LibraryExtension> {
                 configureKotlinAndroid(this)
                 defaultConfig.targetSdk = 33
+                defaultConfig {
+                    consumerProguardFiles("proguard-rules.pro", "consumer-rules.pro")
+                }
             }
 
 //            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
