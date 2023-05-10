c540d3137a975630726ad71e3d6ea62d9a3b7050
chunjinchen <chunjinchen1998@gmail.com>
2023-05-10 15:41:32 +0800

    更新AGP,以及其他的依赖版本

--- convention/src/main/kotlin/cn/chitanda/app/imovie/KotlinAndroid.kt
+++ convention/src/main/kotlin/cn/chitanda/app/imovie/KotlinAndroid.kt
@@ -1,6 +1,7 @@
 package cn.chitanda.app.imovie
 
 import com.android.build.api.dsl.CommonExtension
+import com.android.build.gradle.tasks.computeJavaSource
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionAware
@@ -21,8 +22,8 @@ internal fun Project.configureKotlinAndroid(
         defaultConfig { minSdk = 26 }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
 
         kotlinOptions {
@@ -40,7 +41,7 @@ internal fun Project.configureKotlinAndroid(
             )
 
             // Set JVM target to 11
-            jvmTarget = JavaVersion.VERSION_11.toString()
+            jvmTarget = JavaVersion.VERSION_17.toString()
         }
 //        val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
 //        dependencies {
